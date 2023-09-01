bdb17b953b0fc2b57c95e3da97ce7cc4c39f904c
TomLuuu22 <138814317+TomLuuu22@users.noreply.github.com>
2023-09-01 11:36:59 -0400

    Add files via upload

---
+++ conversion.h
@@ -0,0 +1,25 @@
+#ifndef conversion_h
+#define conversion_h
+
+#include <Arduino.h>
+
+class conversion{
+  public:
+  // convert flexsensor values to displacements
+  float flex2disp(int flex);
+  // convert displacements to flexsensor values (reverse)
+  int disp2flex(float disp);
+  // convert spring stiffness to number of layers
+  float stif2layer(float k);
+  // convert number of layers to  spring stiffness (reverse)
+  float layer2stif(float layers);
+
+  private:
+  float rest_length = -105.8; // equilibrium flex sensor value, converted to [mm]
+  float df_convert = -0.2; // displacement/flex value conversion, [mm]
+  float ls_convert = 137.9; // (half) layers/stiffness conversion, [mm/N]
+  float firstlayer_k = 0.07; // stiffness of layer 1, [N/mm]
+
+};
+
+#endif
